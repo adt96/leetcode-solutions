711d8035cbecbb2bce32926d53dae15ff220fcb6
adt96 <ksaditya005@gmail.com>
2023-09-12 23:53:03 +0530

    twoSum solution added

---
+++ twoSum.java
@@ -0,0 +1,34 @@
+//BRUTE - FORCE
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+                if (nums[i]+nums[j]==target)
+                {
+                    return new int[] {i,j};
+                }
+            }
+
+        }
+return nums;
+    }
+}
+
+
+// USING HASHMAP
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> compliments = new HashMap<>();
+        for(int i=0;i<nums.length;i++)
+        {  Integer complimentIndex =compliments.get(nums[i]);
+           if(complimentIndex != null)
+           {
+               return new int[] {i, complimentIndex};
+           }
+           compliments.put(target- nums[i], i);
+        }
+        return nums;
+    }
+}
