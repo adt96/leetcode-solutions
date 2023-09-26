f27e6e3527ed58292ca4560e5f03f30aad7444f4
Aditya Shiva <35380459+adt96@users.noreply.github.com>
2023-09-26 20:07:43 +0530

    Create countMatches.py
    https://leetcode.com/problems/count-of-matches-in-tournament/

---
+++ countMatches.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n-1
+
+# https://leetcode.com/problems/count-of-matches-in-tournament/
